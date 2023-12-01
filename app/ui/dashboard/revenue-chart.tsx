00557414138a6722416a6c3767815610d951d271
sergio_borges <sluizborges@gmail.com>
2023-12-01 15:40:30 -0300

    Update loading.tsx, page.tsx, and 3 more files...

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,6 +2,8 @@ import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { Revenue } from '@/app/lib/definitions';
 
+import { fetchRevenue } from '@/app/lib/data';
+
 // This component is representational only.
 // For data visualization UI, check out:
 // https://www.tremor.so/
