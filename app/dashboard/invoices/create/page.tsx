18f75ab63b4ebfd4e986a32ad329fd545ab563a5
sergio_borges <sluizborges@gmail.com>
2023-12-05 21:26:14 -0300

    Update page.tsx, actions.ts, and 3 more files...

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -3,7 +3,6 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
  
 export default async function Page() {
-    
   const customers = await fetchCustomers();
  
   return (
